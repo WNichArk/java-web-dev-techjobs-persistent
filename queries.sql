fb0c2aa61cd48c903ae13a36c44bfe4d5280c083
Will Nicholson <william.nicholson.dev@gmail.com>
2021-04-03 11:51:16 -0500

    lunch break

--- queries.sql
+++ queries.sql
@@ -1,7 +1,9 @@
 ## Part 1: Test it with SQL
-
+describe job
 ## Part 2: Test it with SQL
-
+select name
+from employer
+where location = 'St Louis'
 ## Part 3: Test it with SQL
 
 ## Part 4: Test it with SQL
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -2,5 +2,7 @@ package org.launchcode.javawebdevtechjobspersistent.models;
 
 public class Employer extends AbstractEntity {
 
+    @
+    private String location;
 
 }
