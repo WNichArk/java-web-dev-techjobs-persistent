68c0974ae52ac88227c05fd8991bd4b0e9f4f9ea
Will Nicholson <william.nicholson.dev@gmail.com>
2021-04-05 18:22:14 -0500

    i read through that last sql query question way too fast.

--- queries.sql
+++ queries.sql
@@ -7,6 +7,7 @@ where location = 'St Louis City'
 ## Part 3: Test it with SQL
 Drop table job
 ## Part 4: Test it with SQL
-Select name, description
-from skill
-order by name;
+select s.name, s.description from skill s
+left join job_skills js on s.id = js.skills_id
+Where js.jobs_id IS NOT NULL
+Order By 1
